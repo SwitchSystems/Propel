a2c796536ef090882065105651c23f3943ff29cb
Kris Sum <kris@switchsystems.co.uk>
2021-02-11 15:02:55 +0000

    Fixed bug with addJoinCondition on non-top-level queries, which was introduced in upstream 1.7.2.1

--- runtime/lib/query/Join.php
+++ runtime/lib/query/Join.php
@@ -563,14 +563,10 @@ class Join
      */
     public function equals($join)
     {
-        $parametersOfThisClauses = array();
-        $parametersOfJoinClauses = array();
-
         return $join !== null
                 && $join instanceof Join
                 && $this->getJoinType() == $join->getJoinType()
-                && $this->getConditions() == $join->getConditions()
-                && $this->getClause($parametersOfThisClauses) == $join->getClause($parametersOfJoinClauses);
+                && $this->getConditions() == $join->getConditions();
     }
 
     /**
